8319150092cdade2b41ea5b3ad177e23ff237283
Lindokuhle <yendelindokuhle2@gmail.com>
2025-09-02 11:11:28 +0200

    MovieTestDrive.java

--- src/chapter2/MovieTestDrive.java
+++ src/chapter2/MovieTestDrive.java
@@ -8,6 +8,13 @@ class Movie {
     void playIt() {
         System.out.println("Playing the movie");
     }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Genre: " + genre);
+        System.out.println("Rating: " + rating);
+        System.out.println("///////////");
+    }
 }
 
 public class MovieTestDrive {
@@ -17,15 +24,18 @@ public class MovieTestDrive {
         one.genre = "Tragic";
         one.rating = -2;
         one.playIt();
+        one.displayInfo();
         Movie two = new Movie();
         two.title = "Lost in Cubicle Space";
         two.genre = "Comedy";
         two.rating = 5;
         two.playIt();
+        two.displayInfo();
         Movie three = new Movie();
         three.title = "Byte Club";
         three.genre = "Tragic but ultimately uplifting";
         three.rating = 127;
         three.playIt();
+        three.displayInfo();
     }
 }
